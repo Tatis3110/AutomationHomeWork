8c9a804899b95baf476e783a23e1e83d43b957a2
tetiana <tetiana.portiianina@gmail.com>
2021-01-05 19:13:46 +0200

    changes

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,29 +1,43 @@
 package tests;
 
+import org.apache.commons.logging.Log;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 public abstract class BaseTest {
 
     protected static WebDriver driver;
 
     @Before
-    public void setUp(){
-        System.out.println(System.setProperty(
-                "webdriver.chrome.driver",
-                System.getProperty("user.dir") +
-                        "/src/main/resources/drivers.chrome.v85/chromedriver"));
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    public void SetUp() {
+        switch (System.getProperty("browser", "chrome")){
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "opera":
+                driver = new OperaDriver();
+                break;
+            case "ie":
+                driver = new InternetExplorerDriver();
+                break;
+            default:
+                driver = new FirefoxDriver();
+                break;
+        }
+        driver.manage().window().maximize();
         driver.get("https://github.com/login");
     }
 
     @After
-    public void tearDown(){
-
+    public void tearDown() {
+        Log(driver.getCurrentUrl());
+        driver.quit();
     }
 }
